53a0d940de80c1001aa29f111e4bbb565c4d8794
CharltonPoole <charltonpoole46@gmail.com>
2025-11-03 09:21:42 +0200

    first commit

---
+++ basics.js
@@ -0,0 +1,13 @@
+// TODO: Create a variable called firstName and store your first name in it
+let firstName="Charlton"
+// TODO: Create a variable called lastName and store your last name in it
+let lastName="Poole"
+// TODO: Create a variable called age using let (you'll modify this later)
+let age=",I am 21 years old"
+// TODO: Create a fullName variable that combines firstName and lastName
+let fullName="Charlton Poole"
+// TODO: Print a greeting to the console using these variables
+console.log("Hello, my name is",fullName,age)
+// TODO: Try changing the age variable to a different value
+age = ",I am 22 years old"
+ console.log("Hello, my name is",fullName,age)
